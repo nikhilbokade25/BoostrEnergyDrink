9523d212341d31659715fe31067f0e66d497feca
Nikhil Bokade <nikhilbokade25@gmail.com>
2025-04-14 00:02:53 -0400

    Initialise Product Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Hero from "../src/Components/Hero";
+import Products from './Components/Products';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <Products />
     </div>
   );
 }
